268e77df5b7a75970416f37ad6bb4ff5c6fcfe64
vikrant-sahoo <131588977+vikrant-sahoo@users.noreply.github.com>
2024-07-16 19:05:38 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
                   <Navbar></Navbar>
                   <div className="home">
                         <h1 className="home-title">Your Ultimate Contact Management Solution</h1>
-                        <p className="home-details"><span>Contact-Nest</span> offers a secure and efficient solution for managing your contacts. With an user-friendly interface, it helps you keep your connections organized and accessible, making it easier than ever to stay in touch.</p>
+                        <p className="home-details"><span className="home-logo">Contact-Nest</span> offers a secure and efficient solution for managing your contacts. With an user-friendly interface, it helps you keep your connections organized and accessible, making it easier than ever to stay in touch.</p>
                   </div>
             </>
             
